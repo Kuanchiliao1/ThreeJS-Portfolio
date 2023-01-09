b1e55d7aa5841dbf189f2d05e84d1fb4ff4eb65d
kuanchiliao1 <kuanchiliao@gmail.com>
2023-01-09 12:30:37 -0500

    Modify flexbox setup

--- nuxt-portfolio/pages/portfolio.vue
+++ nuxt-portfolio/pages/portfolio.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="h-screen border-white text-white">
-    <header class="background-img-header pb-64 border-b">
+  <div class="body-container--portfolio h-screen border-white text-white">
+    <header class="background-img-header pb-64">
       <nav>
-        <div class="flex justify-between border-b uppercase">
-          <p class="self-center pl-7">Tony Liao</p>
+        <div class="flex justify-between border-b uppercase tracking-wide">
+          <p class="self-center pl-7 font-bold tracking-widest">Tony Liao</p>
           <ul class="right-nav flex">
             <li class="py-5 px-7 border-l">Projects</li>
             <li class="py-5 px-7 border-l">Contact</li>
@@ -13,22 +13,22 @@
       </nav>
       <br>
       <section class="py-8 px-4 mx-auto max-w-4xl text-center">
-        <h1 class="text-6xl uppercase font-bold mb-16 border-b pb-16 mt-32">About Me</h1>
-        <p class="text-xl px-1">
+        <h1 class="text-6xl title-text uppercase font-bold mb-16 border-b pb-16 mt-32 tracking-wide">About Me</h1>
+        <p class="px-1">
           Hi, I'm Tony! I am a frontend developer with a passion for learning, creating, and experimenting. I love the freedom and autonomy that programming offers, and the amazing power to bring my ideas to life through code.
 
           Outside of work, I enjoy exploring my creative side through drone photography, playing the piano, and practicing card magic. I am also excited to be learning ThreeJS on the side, and look forward to learning more about creating animations and visual effects.
         </p>
       </section>
     </header>
-    <main class=" bg-black py-8 px-4 mx-auto max-w-4xl">
-      <section class="flex flex-col py-8 px-4 mx-auto max-w-4xl text-center">
+    <main class="text-black mx-auto max-w-4xl">
+      <section class="flex flex-col py-8 px-12 mx-auto max-w-4xl text-center">
         <h2 class="uppercase bold border-b pb-3">My Projects</h2>
         <div class="flex flex-col">
           <h3 class="mt-4">Autohotkey Script collection</h3>
-          <div class="flex justify-between">
-            <img class="block w-5/12" src="https://i.imgur.com/jCXCdvw.jpeg" alt="########">
-            <div class="w-5/12 flex flex-col justify-between">
+          <div class="flex  flex-row flex-wrap justify-between gap-0.5 text-start">
+            <img class="block w-full md:w-5/12 self-start" src="https://i.imgur.com/jCXCdvw.jpeg" alt="########">
+            <div class="w-full md:w-5/12 flex flex-col justify-between text-start md:text-left">
               <p>Autohotkey is my first language, and it allows for the creation of custom scripts to automate tasks and improve their workflow on Windows computers. I have been lightly working on this project since 2019 and have compiled a large collection of scripts that have helped me automate my programming and regular workflows. This project is ongoing as I continually add new functionality and optimize my existing code. Take a look and see what you can do with Autohotkey!</p>
               <h4>
                 Cool features:
@@ -36,7 +36,10 @@
               <ul>
                 <li>Insta Google</li>
                 <li>rem/px conversion</li>
-                <li></li>
+                <li>asdfa</li>
+                <li>asdfa</li>
+                <li>asdfa</li>
+                <li>asdfa</li>
               </ul>
               <p>Technologies used: Autohotkey, OpenAI API</p>
             </div>
