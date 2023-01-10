83f3adb88ed768d17042e11d78b1f90cb55a0d65
kuanchiliao1 <kuanchiliao@gmail.com>
2023-01-10 18:58:00 -0500

    Add content and video

--- nuxt-portfolio/pages/portfolio.vue
+++ nuxt-portfolio/pages/portfolio.vue
@@ -1,13 +1,12 @@
 <template>
-  <div class="bg-black body-container--portfolio h-screen border-white text-white">
+  <div class="body-container--portfolio h-screen border-white text-white">
     <header class="background-img-header pb-64">
       <nav>
         <div class="flex justify-between border-b uppercase tracking-wide">
           <p class="self-center pl-7 font-bold tracking-widest">Tony Liao</p>
           <ul class="right-nav flex">
-            <li class="py-5 px-7 border-l">Projects</li>
-            <li class="py-5 px-7 border-l">Contact</li>
-            <li class="py-5 px-7 border-l">Filler</li>
+            <li class="py-5 px-7 border-l"><a href="#projects">Projects</a></li>
+            <li class="py-5 px-7 border-l"><a href="">Contact</a></li>
           </ul>
         </div>
       </nav>
@@ -21,58 +20,91 @@
         </p>
       </section>
     </header>
-    <main class="text-black mx-auto max-w-4xl">
-      <section class="bg-white flex flex-col py-8 px-12 mx-auto max-w-4xl text-center">
+    <main id="projects" class="bg-white text-black mx-auto max-w-4xl">
+      <section class="flex flex-col py-8 px-12 mx-auto max-w-4xl text-center">
         <h2 class="uppercase bold border-b pb-3">My Projects</h2>
         <div class="flex flex-col">
-          <h3 class="mt-4">Autohotkey Script collection</h3>
-          <div class="flex  flex-row flex-wrap justify-between gap-0.5 text-start">
-            <img class="block w-full md:w-5/12 self-start" src="https://i.imgur.com/jCXCdvw.jpeg" alt="########">
+          <h3 class="my-4">Autohotkey Script collection</h3>
+          <div class="flex items-start flex-row flex-wrap justify-between gap-0.5 text-start">
+            <!-- <img class="pt-1 block w-full md:w-5/12 self-start" src="https://i.imgur.com/FCuosDc.jpeg" alt="########"> -->
+
+            <video controls class="pt-1 w-full md:w-5/12 justify-between text-start md:text-left" style="font-size: 0;">
+              <source src="../static/Code_9ZurxCxHrh.mp4"
+                      type="video/mp4">
+            </video>
             <div class="w-full md:w-5/12 flex flex-col justify-between text-start md:text-left">
-              <p>Autohotkey is what got me hooked on coding in 2019. It allows for the creation of custom scripts to automate tasks and improve their workflow on Windows computers. I have compiled and wrote a large collection of scripts that have helped me automate my programming and regular workflows. This project is ongoing as I continually add new functionality and optimize my existing code. Take a look and see what you can do with Autohotkey!</p>
-              <h4>
-                Cool features:
+              <p>Autohotkey is what first got me hooked on coding in 2019. It allows for the creation of custom scripts to automate tasks and improve their workflow on Windows computers. I have compiled and wrote a large collection of scripts that have helped me automate my programming and regular workflows. This project is ongoing as I continually add new functionality and optimize my existing code. Take a look and see what you can do with Autohotkey!</p>
+              <h4 class="mt-4">
+                Cool features
               </h4>
               <ul>
-                <li>Insta Google</li>
-                <li>rem/px conversion</li>
-                <li>asdfa</li>
-                <li>asdfa</li>
-                <li>asdfa</li>
-                <li>asdfa</li>
+                <li>- Insta Google</li>
+                <li>- rem/px conversion</li>
+                <li>- A.I. analyzer</li>
               </ul>
-              <p>Technologies used: Autohotkey, OpenAI API</p>
+              <p class="mt-4">Technologies used: Autohotkey, OpenAI API</p>
             </div>
           </div>
         </div>
         <div class="flex flex-col">
-          <h3 class="mt-4">Autohotkey Script collection</h3>
-          <div class="flex justify-between">
-            <img class="block w-5/12" src="https://i.imgur.com/jCXCdvw.jpeg" alt="########">
-            <div class="w-5/12 flex flex-col justify-between">
-              <p>I built this website using HTML, CSS, and JavaScript, and utilized CSS custom properties and utility classes to create a maintainable and reusable codebase. I also took this opportunity to study best practices for accessibility and to gain insights into professional development strategies, including project planning and implementation using a Figma design file.</p>
-              <h4>
-                Cool features: Insta Google, rem/px conversion, stuff
+          <h3 class="my-4">Portfolio Landing Page</h3>
+          <div class="flex  flex-row flex-wrap justify-between gap-0.5 text-start m-0">
+            <img class="pt-1 block w-full md:w-5/12 self-start" src="https://i.imgur.com/FCuosDc.jpeg" alt="########">
+            <div class="w-full md:w-5/12 flex flex-col justify-between text-start md:text-left">
+              <p>I built this portfolio landing page as an introduction to several new topics. Originally created by Chris Lis, I added my own twists and improvements. I'm most proud of a fun feature I implemented called Rainbow Mode. </p>
+              <h4 class="mt-4">
+                Cool features
               </h4>
               <ul>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>- Rainbow mode</li>
+                <li>- Skip to content for accessibility</li>
+                <li>- Orbit controls with mouse</li>
               </ul>
-              <p>Technologies used: Autohotkey, OpenAI API</p>
+              <p class="mt-4">Technologies used: HTML/CSS/JS, Nuxt.js, Vite, Three.js, Tailwind</p>
             </div>
           </div>
         </div>
-        <h3>Autohotkey script collection</h3>
-        <h3>Space Tourism: I built this website using HTML, CSS, and JavaScript, and utilized CSS custom properties and utility classes to create a maintainable and reusable codebase. I also took this opportunity to study best practices for accessibility and to gain insights into professional development strategies, including project planning and implementation using a Figma design file.</h3>
-        <h3>What to Say</h3>
-        <h3>Portfolio?</h3>
-        <p>Hi, I'm Tony! I am a frontend developer with a passion for learning, creating, and experimenting. I love the freedom and autonomy that programming offers, and the amazing power to bring my ideas to life through code.
-
-        Outside of work, I enjoy exploring my creative side through drone photography, playing the piano, and practicing card magic. I am also excited to be learning ThreeJS on the side, and look forward to learning more about creating animations and visual effects.</p>
+        <div class="flex flex-col">
+          <h3 class="my-4">Space Tourism</h3>
+          <div class="flex  flex-row flex-wrap justify-between gap-0.5 text-start">
+            <img class="pt-1 block w-full md:w-5/12 self-start" src="https://i.imgur.com/MQbYl2j.jpeg" alt="########">
+            <div class="w-full md:w-5/12 flex flex-col justify-between text-start md:text-left">
+              <p>I built this website using HTML, CSS, and JavaScript, and utilized CSS custom properties and utility classes to create a maintainable and reusable codebase. I also took this opportunity to study best practices for accessibility and to gain insights into professional development strategies, including project planning and implementation using a Figma design file.</p>
+              <h4 class="mt-4">
+                Cool features
+              </h4>
+              <ul>
+                <li>- Multiple background images for different screen sizes</li>
+                <li>- Keyboard accessibility</li>
+                <li>- Animated button</li>
+                <li>- Fun space facts</li>
+              </ul>
+              <p class="mt-4">Technologies used: HTML/CSS/JS, Github Pages</p>
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-col">
+          <h3 class="my-4">What to Say</h3>
+          <div class="flex  flex-row flex-wrap justify-between gap-0.5 text-start">
+            <img class="pt-1 block w-full md:w-5/12 self-start" src="https://i.imgur.com/6LjHvea.jpeg" alt="########">
+            <div class="w-full md:w-5/12 flex flex-col justify-between text-start md:text-left">
+              <p>This is a personal language learning handbook with the purpose of helping new language learners track phrases and their responses, as well as take notes on them.</p>
+              <p>This is a project mainly focused on the back end. I used Ruby with the Sinatra framework</p>
+              <h4 class="mt-4">
+                Cool features
+              </h4>
+              <ul>
+                <li>- Persists data through PSQL database</li>
+                <li>- Multiple simultaneous validations of Entry inputs</li>
+                <li>- </li>
+              </ul>
+              <p class="mt-4">Technologies used: Ruby, Sinatra, PSQL, Heroku, HTML/CSS</p>
+            </div>
+          </div>
+        </div>
       </section>
       <section>
-        <h2 class="text-2xl font-bold text-gray-800 mb-4">Contact Me</h2>
+        <h2 class="text-center text-2xl font-bold text-gray-800 mb-4">Contact Me</h2>
         <form class="w-full max-w-lg mx-auto"   action="#" method="post">
           <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
