7a268d01d2e3268e0f175790a601a129b5867263
kuanchiliao1 <kuanchiliao@gmail.com>
2023-01-11 20:58:43 -0500

    Change title

--- nuxt-portfolio/nuxt.config.js
+++ nuxt-portfolio/nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'my-portfolio-nuxt',
+    title: 'tony-portfolio',
     htmlAttrs: {
       lang: 'en'
     },
