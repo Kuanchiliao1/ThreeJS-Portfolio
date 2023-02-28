fc1be797944d98aa6c5bcfb67f9345e94345f67f
kuanchiliao1 <kuanchiliao@gmail.com>
2023-02-28 10:48:19 -0500

    Remove iframe element

--- nuxt-portfolio/pages/portfolio.vue
+++ nuxt-portfolio/pages/portfolio.vue
@@ -252,7 +252,6 @@
           <br>
           <br>
         </div>
-        <iframe src="https://www.w3schools.com" title="W3Schools Free Online Web Tutorials"></iframe>
       </footer>
   </div>
 </template>
